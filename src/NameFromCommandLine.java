b3aa5ab945d72722df03cb251b20931458815669
Raaj <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>
2022-09-21 18:37:19 +0530

    final commit

---
+++ src/NameFromCommandLine.java
@@ -0,0 +1,6 @@
+public class NameFromCommandLine {
+    public static void main(String[] args) {
+        String name = args[0];
+        System.out.println("the entered command line argument is --> "+name);
+    }
+}
